da3fe152aa0d400f796560c1acd25e29ddd3ebd0
sreganesh20 <65004166+sreganesh20@users.noreply.github.com>
2020-05-08 10:31:09 +0530

    swap.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 2 numbers:\n ");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("\na: %d b: %d\n",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\n After swap a: %d b: %d\n",a,b);
+
+}
